7d28e17df1a9cc600cd33bd9c94f76e08815d538
jnelaw <103033243+jnelaw@users.noreply.github.com>
2023-08-07 21:13:53 +0200

    update recipies

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   // Configure Content Wind here
-  cover: '/cover.jpg',
+  cover: '/BakenrugFarm_1 (Medium).jpg',
   socials: {
     twitter: 'Atinux',
     github: 'Atinux/content-wind'
